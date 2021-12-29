80dc8895d4adbff628dea3738d544db7db749a8a
yashar rezvani <yasharrezvan98@gmail.com>
2021-12-29 18:58:55 +0800

    changes

---
+++ accordion.js
@@ -0,0 +1,107 @@
+
+/*let accordiyon = document.querySelector(".accordion");
+
+Array.from(accordiyon.children).forEach(wrapper =>{
+    if (wrapper.classList.contains("show")) {
+		wrapper.querySelector("div").style.maxHeight = wrapper.querySelector("div").scrollHeight + 30 + 'px'
+	}
+    wrapper.querySelector("span").addEventListener("click", e =>{
+        let span = e.target;
+        let wrapper = span.parentElement;
+        let div = span.nextElementSibling;
+        wrapper.classList.toggle("show");
+        if (wrapper.classList.contains("show")) {
+            div.style.maxHeight = div.scrollHeight + 30 + 'px'
+        }else{
+            div.style.maxHeight = null
+        }
+        Array.from(accordiyon.children).forEach(w =>{
+            if (w != wrapper) {
+                w.classList.remove("show");
+                w.querySelector("div").style.maxHeight = null
+            }
+            
+        }) 
+		
+    } )
+})*/
+
+let accordiyon =  document.querySelector('.accordion');
+
+
+Array.from(accordiyon.children).forEach(wrapper =>{
+    if(wrapper.classList.contains('show')){
+        wrapper.querySelector('div').style.maxHeight = wrapper.querySelector('div').scrollHeight + 30 + 'px'
+    }
+       wrapper.querySelector('span').addEventListener('click', e =>{
+           let span = e.target
+           let wrapper = span.parentElement
+           let div = span.nextElementSibling
+           wrapper.classList.toggle("show")
+           if(wrapper.classList.contains('show')){
+               div.style.maxHeight= div.scrollHeight + 30 + 'px'
+           }
+           else{
+               div.style.maxHeight = null
+           }
+           Array.from(accordiyon.children).forEach( w=>{
+            if(w != wrapper){
+                w.classList.remove('show');
+                w.querySelector('div').style.maxHeight=null
+            }
+        })
+        
+       })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
